d1b960cb2cec4a2c613311a7a34b096a009c29e3
Alvin Bundalian <alvinbundalian@Alvins-Mac-mini.attlocal.net>
2023-04-24 22:47:33 -0700

    formatting

--- src/discord/events/guildBanAdd.ts
+++ src/discord/events/guildBanAdd.ts
@@ -2,13 +2,9 @@ import { GuildBan } from 'discord.js';
 import { BotsProps } from 'src/interfaces';
 import { logEvent } from '../../utils';
 
-export const onGuildBanAdd = async (    
-  Bots: BotsProps,
-  guildBan: GuildBan
-) => {
-
+export const onGuildBanAdd = async (Bots: BotsProps, guildBan: GuildBan) => {
   const { user, reason } = guildBan;
-  const reasonStr = reason? `\nReason: ${reason}`: '';
+  const reasonStr = reason ? `\nReason: ${reason}` : '';
 
   logEvent({
     Bots,
@@ -18,23 +14,25 @@ export const onGuildBanAdd = async (
     footer: `Discord User ID: ${user.id}`,
   });
 
-  await Bots.db?.collection(Bots.env.MONGODB_USERS).findOneAndDelete(
-    { discord_id: user.id },
-  ).then(()=> {
-    logEvent({
+  await Bots.db
+    ?.collection(Bots.env.MONGODB_USERS)
+    .findOneAndDelete({ discord_id: user.id })
+    .then(() => {
+      logEvent({
         Bots,
         type: 'delete',
         description: `Record with discord_id=${user.tag} has been removed from collection ${Bots.env.MONGODB_USERS}.`,
         thumbnail: user.displayAvatarURL() || undefined,
         footer: `Discord User ID: ${user.id}`,
       });
-  }).catch(() => {
-    logEvent({
+    })
+    .catch(() => {
+      logEvent({
         Bots,
         type: 'error',
         description: `Error deleting record with discord_id=${user.tag} from collection ${Bots.env.MONGODB_USERS}.`,
         thumbnail: user.displayAvatarURL() || undefined,
         footer: `Discord User ID: ${user.id}`,
       });
-  });
+    });
 };
