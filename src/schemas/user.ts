0cb600e570afbfa51ecea8f832c5fa8364e91879
Athena <athena@parthenon.app>
2023-05-08 00:42:07 -0700

    feat(schemas): [OWL-27002516] Added data model for users

---
+++ src/schemas/user.ts
@@ -0,0 +1,12 @@
+export const UserModel = {
+  user_id: null,
+  discord_id: null,
+  discord_username: null,
+  twitch_id: null,
+  twitch_username: null,
+  accounts_linked: false,
+  cash: 0,
+  bank: 0,
+  stars: 0,
+  power_ups: [],
+};
