ea4d422386d1a36aa633b4abb369b7106d181107
Athena <athena@parthenon.app>
2025-08-02 12:09:25 -0700

    refactor: [OWL-00010] Updated the emojis with the ones uploaded for Little Owl

--- src/constants/emojis.ts
+++ src/constants/emojis.ts
@@ -1,6 +1,6 @@
 export const EMOJIS = {
-  CURRENCY: '<:silver:1370526592375197797>',
-  CURRENCY_TAILS: '<:silver_tails:1370526609496342528>',
+  CURRENCY: '<:silver:1401279198571790346>',
+  CURRENCY_TAILS: '<:silver_reverse:1401279283040620606>',
   CUSTOM: {
     // animated emojis have an extra "a" character before the name
     ARRIVE: '<a:athena1Arrive:1251686415603863562>',
